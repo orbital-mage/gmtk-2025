943a95b862de34e96146073faea70614cff49335
orbital-mage <nitzanedri1@gmail.com>
2025-07-31 01:53:34 +0300

    scrolling

--- phone/phone.gd
+++ phone/phone.gd
@@ -1,43 +1,27 @@
-extends Node2D
+extends ScrollContainer
 
 var video_scene = preload("res://phone/video.tscn")
 
-var current_video: PathFollow2D
-var next_video: PathFollow2D
-var videos: Array[PathFollow2D]
-
+var current_video: Control
 var scroll := false
 
+@onready var container: Container = $VBoxContainer
+
 func _ready() -> void:
-	current_video = _create_video()
-	current_video.progress_ratio = 0.5
-	
-	next_video = _create_video()
-	next_video.progress_ratio = 0
+	_new_video()
+	_new_video()
 
 func _process(delta: float) -> void:
-	if current_video.progress_ratio == 1:
-		videos.remove_at(0)
-		current_video.queue_free()
-		current_video = next_video
-		next_video = _create_video()
-		scroll = false
-	
-	State.snooze(delta)
-	
 	if scroll:
-		for video in videos:
-			if video.progress_ratio < 1:
-				video.progress_ratio += delta
+		scroll_vertical += delta * 800
+		if scroll_vertical == size.y:
+			print('end')
+			scroll = false
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_accept"):
+	if event.is_action_pressed("scroll"):
 		scroll = true
-		State.focus()
 
-func _create_video() -> PathFollow2D:
-	var video = video_scene.instantiate() as PathFollow2D
-	
-	add_child(video)
-	videos.append(video)
-	return video
+func _new_video() -> void:
+	var video = video_scene.instantiate()
+	container.add_child(video)
