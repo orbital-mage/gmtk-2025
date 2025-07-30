e8839e06cabda98a70fb5ae5abe42d2a650f39a5
orbital-mage <nitzanedri1@gmail.com>
2025-07-31 00:18:53 +0300

    demon

--- world/demon.gd
+++ world/demon.gd
@@ -1,6 +1,11 @@
-extends Node2D
+extends CharacterBody2D
 
 @export var player: Node2D
 
+@onready var sprite: Sprite2D = $Demon
+
 func _physics_process(delta: float) -> void:
+	if position.x - player.position.x > 0:
+		sprite.flip_h = true
+	
 	position = position.move_toward(player.position, delta * 100)
