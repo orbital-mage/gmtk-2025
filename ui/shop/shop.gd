3ff669302b87f90cb24e8b8a97335081e610ea21
orbital-mage <nitzanedri1@gmail.com>
2025-08-03 00:06:45 +0300

    no duplicates

--- ui/shop/shop.gd
+++ ui/shop/shop.gd
@@ -1,5 +1,7 @@
 extends Control
 
+static var item_table: ItemTable = load("res://ui/shop/item_table.tres")
+
 @onready var shelf: HBoxContainer = $MarginContainer/Shelf
 
 func _ready() -> void:
@@ -10,10 +12,19 @@ func _on_open(round_number: int) -> void:
 	show()
 	
 	for i in range(3):
-		shelf.add_child(ShopOption.random_item(round_number))
+		shelf.add_child(ShopOption.create(round_number, _random_item()))
 
 func _on_close() -> void:
 	hide()
 	
 	for option in shelf.get_children():
 		shelf.remove_child(option)
+
+func _random_item() -> ItemResource:
+	var item = item_table.items[randi_range(0, item_table.items.size() - 1)]
+	
+	for option: ShopOption in shelf.get_children():
+		if option.item == item:
+			return _random_item()
+	
+	return item
