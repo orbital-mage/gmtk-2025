88e7f5fa90c2d75c74f69b50beb0d783461c4732
orbital-mage <nitzanedri1@gmail.com>
2025-08-02 16:18:50 +0300

    teleport raycast

--- game/items/teleport.gd
+++ game/items/teleport.gd
@@ -1,4 +1,14 @@
 class_name Teleport extends Item
 
 func use(clone: Clone, target: Vector2) -> void:
-	clone.position = target
+	var space_state = clone.get_world_2d().direct_space_state
+	var query = PhysicsRayQueryParameters2D.create(
+		clone.global_position, 
+		target, 
+		Collision.create_mask([Collision.Layers.BOUNDS]))
+	var result = space_state.intersect_ray(query)
+	
+	if not result:
+		clone.position = target
+	else:
+		clone.position = result.position
--- game/utils/collision.gd
+++ game/utils/collision.gd
@@ -4,5 +4,16 @@ enum Layers {
 	CLONES = 1,
 	POWERUPS = 2,
 	ZOMBIES = 3,
-	BULLETS = 4 
+	BULLETS = 4,
+	BOUNDS = 9,
+	ENVIRONMENT = 10
 }
+
+static func create_mask(values: Array[int]) -> int:
+	var obj = StaticBody2D.new()
+	obj.collision_mask = 0
+	
+	for value in values:
+		obj.set_collision_mask_value(value, true)
+	
+	return obj.collision_mask
