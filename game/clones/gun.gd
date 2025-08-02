222c1e7acad247cfeaeb86877e9f349a9d6c4a9a
orbital-mage <nitzanedri1@gmail.com>
2025-08-02 20:59:37 +0300

    gun pivot

--- game/clones/gun.gd
+++ game/clones/gun.gd
@@ -1,10 +1,11 @@
 class_name CloneGun extends Node2D
 
-@onready var sprite: Sprite2D = $Gun
+@onready var pivot: Node2D = $Pivot
+@onready var sprite: Sprite2D = $Pivot/Sprite
 @onready var discard_particle: GPUParticles2D = $Discard
 
 func aim(point: Vector2) -> void:
-	look_at(point)
+	pivot.look_at(point)
 
 func get_barrel_position() -> Vector2:
 	return sprite.global_position
