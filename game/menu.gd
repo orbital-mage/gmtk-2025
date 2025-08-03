9ca5788c63f5b4e46f4810d9e3a7ce94d9d6f2a8
Pyrocreep <pyrocreeperking@gmail.com>
2025-08-03 14:29:07 +0200

    splash screen

---
+++ game/menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+@onready var test_sound: AudioStreamPlayer = $TestSound
+
+func set_volume(value: float):
+	AudioServer.set_bus_volume_db(0, lerp(-70.0, 20.0, value))
+	test_sound.play()
+
+func start():
+	get_tree().change_scene_to_file("res://game/arena.tscn")
