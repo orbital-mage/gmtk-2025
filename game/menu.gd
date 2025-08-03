bc9cac6da0480b5b2f0b3a20938b2d2ff1c91796
Pyrocreep <pyrocreeperking@gmail.com>
2025-08-03 17:34:54 +0200

    menu adjustment

--- game/menu.gd
+++ game/menu.gd
@@ -2,6 +2,9 @@ extends Control
 
 @onready var test_sound: AudioStreamPlayer = $TestSound
 
+func _ready() -> void:
+	set_volume(0.7)
+
 func set_volume(value: float):
 	AudioServer.set_bus_volume_db(0, lerp(-70.0, 20.0, value))
 	test_sound.play()
